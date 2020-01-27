c8965efffbe0783f7d5dbf8928f96c4a6196fcf8
CameronPennington <cameron.pennington@gmail.com>
2020-01-27 13:59:40 -0500

    CORS set up

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -14,8 +14,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        #self.database_name = "trivia_test"
+        self.database_path = "postgres://postgres:postgres@localhost:5432/trivia_test"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,6 +24,13 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
+
+        self.new_question = {
+            'text': 'Test Question',
+            'answer': 'Test Answer',
+            'difficulty': 1,
+            'category': 1
+        }
     
     def tearDown(self):
         """Executed after reach test"""
